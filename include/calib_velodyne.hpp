bebf4b030798d5761c99083b8aa78b434031d7d0
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2019-11-30 14:42:26 +0000

    add calib_velodyne

---
+++ include/calib_velodyne.hpp
@@ -0,0 +1,58 @@
+#ifndef CALIB_VELO_H
+#define CALIB_VELO_H
+
+#include <ros/ros.h>
+#include <std_msgs/Int16.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <pcl/point_types.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <collect_calib_data/rotation_broadcasterAction.h>
+
+namespace CalibraionVelodyne
+{
+    class CalibVelo {
+
+    private:
+
+        actionlib::SimpleActionClient<collect_calib_data::rotation_broadcasterAction> client;
+        collect_calib_data::rotation_broadcasterResultConstPtr result;
+        collect_calib_data::rotation_broadcasterGoal goal;
+
+        unsigned int count;
+        bool send_degree;
+        bool success_set_ts;
+        int degree;
+
+        std::string file_path;
+        std::string file_name;
+
+        std::string lis_header_id;
+        std::string lis_child_id;
+
+        std::string cloud_frame;
+
+        ros::Subscriber cloud_sub;
+        ros::Publisher degree_pub;
+        std_msgs::Int16 pub_degree;
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+
+        void get_params();
+        void get_ts(
+            geometry_msgs::TransformStamped &ts);
+        void getpc2_cb(
+            const sensor_msgs::PointCloud2 &cloud_msgs);
+
+    public:
+
+        CalibVelo(ros::NodeHandle &nh);
+        ~CalibVelo();
+        void run();
+
+    };
+}
+
+#endif
---
+++ src/calib_velodyne.cpp
@@ -0,0 +1,162 @@
+#include <ros/ros.h>
+#include <std_msgs/Int16.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <tf2/transform_datatypes.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <pcl_ros/transforms.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/pcd_io.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <collect_calib_data/rotation_broadcasterAction.h>
+
+#include "./../include/calib_velodyne.hpp"
+
+namespace CalibraionVelodyne
+{
+    CalibVelo::CalibVelo(ros::NodeHandle &nh) : 
+        cloud (new pcl::PointCloud<pcl::PointXYZ>()),
+        cloud_frame ("/velodyne_points"),
+        count (0),
+        client ("/rotation_broadcaster", true),
+        degree_pub (nh.advertise<std_msgs::Int16>("servo", 10)),
+        cloud_sub(nh.subscribe(cloud_frame, 1, &CalibVelo::getpc2_cb, this))
+    {
+        get_params();
+        if(!client.waitForServer(ros::Duration(20.0)))
+        {
+            ROS_INFO("Could not find server : rotation_broadcaster");
+            ros::shutdown();
+        }
+    }
+
+    CalibVelo::~CalibVelo()
+    {
+    }
+
+    void CalibVelo::get_params()
+    {
+        ros::param::get("/use_velo/lis_header_id", lis_header_id);
+        ros::param::get("/use_velo/lis_child_id", lis_child_id);
+    
+        ros::param::get("/use_velo/file_path", file_path);
+        ros::param::get("/use_velo/file_name", file_name);
+
+        ros::param::get("/calib_velo/degree", degree);
+    }
+
+
+    void CalibVelo::get_ts(
+        geometry_msgs::TransformStamped &ts)
+    {
+        goal.enable_work.data = true;
+        client.sendGoal(goal);
+
+        bool finished = client.waitForResult(ros::Duration(2.0));
+        if(!finished)
+        {
+            ROS_WARN("rotation broadcaster TimeOut");
+            success_set_ts = false;
+
+            ts.transform.rotation.x = 0.0;
+            ts.transform.rotation.y = 0.0;
+            ts.transform.rotation.z = 0.0;
+            ts.transform.rotation.w = 0.0;
+            ts.transform.translation.x = 0.0;
+            ts.transform.translation.y = 0.0;
+            ts.transform.translation.z = 0.0;
+        }
+        else
+        {
+            success_set_ts = true;
+            result = client.getResult();
+            ts = result->result_rotation;
+        }
+    }
+
+    void CalibVelo::getpc2_cb(
+        const sensor_msgs::PointCloud2 &pc2)
+    {
+        sensor_msgs::PointCloud2 pc2_transformed;
+        geometry_msgs::TransformStamped ts;
+        Eigen::Matrix4f R;
+
+        if(!send_degree)
+        {
+            ROS_INFO("Degree : %d", degree);
+        }
+        else
+        {
+            ROS_INFO("Degree : %d", degree);
+
+            get_ts(ts);
+            if(!success_set_ts)
+            {
+                ROS_WARN("Could not set ts");
+            }
+            else
+            {
+                R = tf2::transformToEigen(ts.transform).matrix().cast<float>();
+
+                pcl_ros::transformPointCloud(R, 
+                                             pc2, 
+                                             pc2_transformed);
+
+                pcl::fromROSMsg(pc2_transformed, *cloud);
+
+                std::string format = ".pcd";
+                std::string savename = file_path 
+                                     + file_name 
+                                     + std::to_string(count) 
+                                     + format; 
+        
+                pcl::io::savePCDFileASCII(savename, *cloud);
+            }
+        }
+    }
+
+       
+    void CalibVelo::run()
+    {
+        ros::Rate rate(1.0);
+        pub_degree.data = 1000.0;
+
+        while(ros::ok())
+        {
+            ros::param::get("/calib_velo/degree", degree);
+            if(degree != pub_degree.data)
+            {
+                pub_degree.data = degree;
+                degree_pub.publish(pub_degree);
+                ros::Duration(7.0);
+                send_degree = true;
+            }
+            else
+            {
+                ros::Duration(2.0);
+                send_degree = false;
+                continue;
+            }
+            rate.sleep();
+            ros::spinOnce();
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "superimpose_velo_points");
+    ros::NodeHandle nh;
+
+    CalibraionVelodyne::CalibVelo *get_pcl;
+    get_pcl = new CalibraionVelodyne::CalibVelo(nh);
+    get_pcl->run();
+
+    delete get_pcl; 
+   
+    return 0;
+}
